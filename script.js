1cbf51d0d60d31677e7146f3f70be8b09ab397f1
Olatomiwa Famutimi <famutimiolatomiwa7@gmail.com>
2025-09-19 17:59:13 +0000

    Day 3: Added JavaScript intro practice file

---
+++ script.js
@@ -0,0 +1,12 @@
+// Day 3 JavaScript practice 🚀
+
+// Log a message in the browser console
+console.log("Hello, Olatomiwa! 🎉 You’ve started JavaScript!");
+
+// Example: Show an alert when the page loads
+alert("Welcome to my portfolio site!");
+
+// Example: Simple function
+function greet() {
+    console.log("Thanks for visiting my portfolio!");
+}
