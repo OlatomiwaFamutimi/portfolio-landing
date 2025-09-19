5e6c7cd2a36a75204250b59c0620a706369bf495
Olatomiwa Famutimi <famutimiolatomiwa7@gmail.com>
2025-09-19 18:26:21 +0000

    Ádded test JaVaScript alert

--- script.js
+++ script.js
@@ -9,4 +9,7 @@ alert("Welcome to my portfolio site!");
 // Example: Simple function
 function greet() {
     console.log("Thanks for visiting my portfolio!");
-}
+}console.log("JavaScript is working!");
+alert("Welcome to my portfolio!");// Test script to confirm JavaScript is linked
+alert("Hello Olatomiwa! Your JavaScript file is working 🎉");
+console.log("JavaScript is connected and running!");
