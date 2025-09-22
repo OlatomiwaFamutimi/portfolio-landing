7d8b223a599a90da617e0aa053a5e4388595afae
Olatomiwa Famutimi <famutimiolatomiwa7@gmail.com>
2025-09-22 17:48:30 +0000

    Added Generate Numbers button and functionality

--- script.js
+++ script.js
@@ -17,4 +17,16 @@ toggleBtn.addEventListener("click", () => {
 const welcomeBtn = document.getElementById("welcomeBtn");
 welcomeBtn.addEventListener("click", () => {
   alert("Hello Olatomiwa! Thanks for visiting my portfolio 🚀");
+});// Button to generate numbers dynamically
+const generateBtn = document.getElementById("generateBtn");
+const numberList = document.getElementById("numberList");
+
+generateBtn.addEventListener("click", () => {
+  numberList.innerHTML = ""; // clear old numbers before generating new ones
+
+  for (let i = 1; i <= 10; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Number " + i;
+    numberList.appendChild(li);
+  }
 });
