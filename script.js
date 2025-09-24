1e2ce001e0c0503363180806edf8c5c8b1c84aaf
Olatomiwa Famutimi <famutimiolatomiwa7@gmail.com>
2025-09-24 19:07:07 +0000

    Added JavaScript Demos,add day switch button

--- script.js
+++ script.js
@@ -41,4 +41,37 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+// Switch statement demo
+daySwitchBtn.addEventListener("click", () => {
+  const dayNumber = prompt("Enter a number (1-7):");
+  let dayName;
+
+  switch (parseInt(dayNumber)) {
+    case 1:
+      dayName = "Sunday";
+      break;
+    case 2:
+      dayName = "Monday";
+      break;
+    case 3:
+      dayName = "Tuesday";
+      break;
+    case 4:
+      dayName = "Wednesday";
+      break;
+    case 5:
+      dayName = "Thursday";
+      break;
+    case 6:
+      dayName = "Friday";
+      break;
+    case 7:
+      dayName = "Saturday";
+      break;
+    default:
+      dayName = "Invalid number! Please enter 1–7.";
+  }
+
+  message.textContent = Result: ${dayName};
+});
 
